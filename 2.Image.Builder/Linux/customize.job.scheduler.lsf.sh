76f68e4408c45955c26c83343073924b73418f38
Rick Shahid <rick.shahid@live.com>
2024-10-22 10:38:51 -0700

    Simplified module configuration

--- 2.Image.Builder/Linux/customize.job.scheduler.lsf.sh
+++ 2.Image.Builder/Linux/customize.job.scheduler.lsf.sh
@@ -19,11 +19,11 @@ if [ $machineType != Storage ]; then
 
   dnf -y install libnsl ed
   cd lsf10*
+  sed -i "/# LSF_ADMINS=/c\LSF_ADMINS=root" install.config
   sed -i "/# LSF_TOP=/c\LSF_TOP=$installRoot" install.config
-  sed -i "/# LSF_ADMINS=/c\LSF_ADMINS=$adminUsername" install.config
+  sed -i "/# LSF_CLUSTER_NAME=/c\LSF_CLUSTER_NAME=Default" install.config
   sed -i "/# LSF_MASTER_LIST=/c\LSF_MASTER_LIST=$(hostname)" install.config
   sed -i "/# LSF_TARDIR=/c\LSF_TARDIR=$binDirectory/lsfs*/lsf" install.config
-  sed -i "/# LSF_CLUSTER_NAME=/c\LSF_CLUSTER_NAME=Default" install.config
   sed -i "/# LSF_SILENT_INSTALL_TARLIST=/c\LSF_SILENT_INSTALL_TARLIST=ALL" install.config
   sed -i "/# SILENT_INSTALL=/c\SILENT_INSTALL=Y" install.config
   sed -i "/# ACCEPT_LICENSE=/c\ACCEPT_LICENSE=Y" install.config
