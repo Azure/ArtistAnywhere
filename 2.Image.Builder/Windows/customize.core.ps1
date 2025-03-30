032ea37486309fc345c35cb29b9848c453a3fed1
Rick Shahid <rick.shahid@live.com>
2025-03-30 08:32:00 -0700

    Simplified module configuration

--- 2.Image.Builder/Windows/customize.core.ps1
+++ 2.Image.Builder/Windows/customize.core.ps1
@@ -75,10 +75,10 @@ if ($machineType -eq "Scheduler") {
   dism /Online /NoRestart /LogPath:"$binDirectory\$fileType" /Enable-Feature /FeatureName:ClientForNFS-Infrastructure /All
   Write-Host "Customize (End): NFS Client"
 
-  # Write-Host "Customize (Start): AD Tools"
-  # $fileType = "ad-tools" # RSAT: Active Directory Domain Services and Lightweight Directory Services Tools
-  # dism /Online /NoRestart /LogPath:"$binDirectory\$fileType" /Add-Capability /CapabilityName:Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
-  # Write-Host "Customize (End): AD Tools"
+  Write-Host "Customize (Start): AD Tools"
+  $fileType = "ad-tools" # RSAT: Active Directory Domain Services and Lightweight Directory Services Tools
+  dism /Online /NoRestart /LogPath:"$binDirectory\$fileType" /Add-Capability /CapabilityName:Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
+  Write-Host "Customize (End): AD Tools"
 
   Write-Host "Customize (Start): Cinebench"
   $version = "2024"
